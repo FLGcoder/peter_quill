de926c899a30064f08dcf764ab808eb49e03cc99
Fernando Luigi Gadotti <fernando_gadotti@estudante.sesisenai.org.br>
2026-05-03 21:01:48 -0300

    add - finalização trabalho

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,33 @@
 const btn = document.getElementById("btnMusica");
 const audio = document.getElementById("musica");
 
-btn.addEventListener("click", () => {
-    if (audio.paused) {
-        audio.play();
-        btn.innerText = "⏸ Pause";
-    } else {
-        audio.pause();
-        btn.innerText = "▶ Play";
-    }
-});
+if (btn && audio) {
+    btn.addEventListener("click", () => {
+        if (audio.paused) {
+            audio.play();
+            btn.innerText = "⏸ Pause";
+        } else {
+            audio.pause();
+            btn.innerText = "▶ Play";
+        }
+    });
+}
 
-/* ======================= ------ ======================= */
+/* ======================= Login ======================= */
+
+const loginForm = document.querySelector('#body-login form');
+
+if (loginForm) {
+    loginForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const email = document.getElementById('floatingInput').value;
+        const senha = document.getElementById('floatingPassword').value;
+
+        if (email === 'admin@gmail.com' && senha === '1234') {
+            window.location.href = 'home.html';
+        } else {
+            alert('Email ou senha incorretos!');
+        }
+    });
+}
